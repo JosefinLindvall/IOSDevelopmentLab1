7f7499ecd76f74a6b52badcd39d41f45f0614bac
JosefinLindvall <lijo18zu@student.ju.se>
2019-10-23 16:40:34 +0200

    finishedLab1

---
+++ HelloSingleView/HelloSingleView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Josefin Lindvall on 2019-10-23.
+//  Copyright © 2019 JosefinLindvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func printHello(_ sender: Any)
+    {
+        print("Hello button")
+    }
+    
+}
+
